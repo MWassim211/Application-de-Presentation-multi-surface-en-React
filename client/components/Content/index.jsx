1b9b6b3ae34587c14bcb9ac1e3f1ae031787847a
Wassim <mohamedwassil.beldjillali@gmail.com>
2020-09-13 19:11:20 +0200

    image added to content component

--- client/components/Content/index.jsx
+++ client/components/Content/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import LogoUniv from '../../assets/logoUniv.png';
 
 export default function Content() {
   return (
     <div>
       Bonjour, Bienvenue au TP1 de l&apos;UE Technologies
       Web synchrones et multi-dispositifs
+      <img src={LogoUniv} alt="Logo Université Claude Bernard" />
     </div>
   );
 }
