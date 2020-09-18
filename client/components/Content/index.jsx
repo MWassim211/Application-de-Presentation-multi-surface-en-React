832f02063099aa732340421956d4cca264b9d9a1
Wassim <mohamedwassil.beldjillali@gmail.com>
2020-09-18 11:53:50 +0200

    content compoenent modified

--- client/components/Content/index.jsx
+++ client/components/Content/index.jsx
@@ -4,7 +4,7 @@ import LogoUniv from '../../assets/logoUniv.png';
 export default function Content() {
   return (
     <div>
-      Bonjour, Bienvenue à l'université claude bernard Lyon 1
+      Bonjour, Bienvenue à l'université claude bernard Lyon 1.
       <img src={LogoUniv} alt="Logo Université Claude Bernard" />
     </div>
   );
