9da5f64263a80ace1c64bebd7119e6e1eb308ea7
Wassim <mohamedwassil.beldjillali@gmail.com>
2020-10-09 12:29:43 +0200

    file added

--- client/components/Header/index.jsx
+++ client/components/Header/index.jsx
@@ -1,13 +1,11 @@
+/* eslint-disable */
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar } from 'react-bootstrap';
+
 
 export default function Header() {
   return (
     <div>
-      <Navbar bg="light">
-        <Navbar.Brand>TP1 - TIW 8</Navbar.Brand>
-      </Navbar>
+      <h1>TP1 - TIW 8</h1>
       Bonjour,  Bienvenue au TP1 de l&apos;UE Technologies
       Web synchrones et multi-dispositifs
     </div>
--- client/index.jsx
+++ client/index.jsx
@@ -1,10 +1,13 @@
+/* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header/index';
 import Content from './components/Content/index';
+import AppToolbar from './components/AppToolbar'
 
 const Index = () => (
   <div className="container">
+    <AppToolbar></AppToolbar>
     <Header />
     <Content />
   </div>
